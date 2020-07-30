0ebb16f70391e88a85e36204d292b14d017122ef
cn-hideyoshi <645165778@qq.com>
2020-07-30 23:42:44 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,47 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+        <view>
+            <text class="title">{{title}}</text>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+    export default {
+        data() {
+            return {
+                title: 'uni-app'
+            }
+        },
+        onLoad() {
 
-		},
-		methods: {
-
-		}
-	}
+        },
+        methods: {}
+    }
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="less">
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+    .logo {
+        height: unit(200, rpx);
+        width: unit(200, rpx);
+        margin: unit(200, rpx) auto unit(50, rpx) auto;
+    }
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+    .text-area {
+        display: flex;
+        justify-content: center;
+    }
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+    .title {
+        font-size: unit(36, rpx);
+        color: #8f8f94;
+    }
 </style>
